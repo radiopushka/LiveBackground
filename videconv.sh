841e180d580830d6ed38be6acd2903a50f2e99d5
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2024-09-25 13:50:54 -0700

    added ffmpeg higher quality options

--- videconv.sh
+++ videconv.sh
@@ -14,7 +14,7 @@ cd "$1.output"
 
 ffmpeg -i "$1" -vcodec h264 -vf scale=$DISPLAY_HEIGHT:$DISPLAY_WIDTH output.mp4
 rm "$1"
-ffmpeg -i output.mp4 -vcodec h264 -filter:v fps=10 output2.mp4
-rm output.mp4
-ffmpeg -i output2.mp4 -pix_fmt rgba %04d.png
+ffmpeg -i output.mp4 -vcodec h264 -qscale:v 1 -filter:v fps=20 output2.mp4
+ffmpeg -i output.mp4 -qmin 1 -q:v 1 -pix_fmt rgba %04d.png
 rm output2.mp4
+rm output.mp4
