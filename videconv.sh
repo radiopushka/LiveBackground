7bc3bdf8213333fd02b26165a50b01683b23ccd4
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2024-09-26 00:11:23 +0000

    Add files via upload

--- videconv.sh
+++ videconv.sh
@@ -12,9 +12,7 @@ mkdir "$1.output"
 cp "$1" "$1.output"
 cd "$1.output"
 
-ffmpeg -i "$1" -vcodec h264 -vf scale=$DISPLAY_HEIGHT:$DISPLAY_WIDTH output.mp4
+ffmpeg -i "$1" -vf scale=$DISPLAY_HEIGHT:$DISPLAY_WIDTH output.gif
 rm "$1"
-ffmpeg -i output.mp4 -vcodec h264 -qscale:v 1 -filter:v fps=20 output2.mp4
-ffmpeg -i output.mp4 -qmin 1 -q:v 1 -pix_fmt rgba %04d.png
-rm output2.mp4
-rm output.mp4
+ffmpeg -i output.gif -pix_fmt rgba %04d.png
+rm output.gif
